488f2e79e8078ae4c69b2f7459bf2fa8bfb6f98f
Suzan Sucro <suzansucro@gmail.com>
2017-01-18 11:49:41 -0800

    Add name, city, and state to user model and user signup forms

---
+++ db/migrate/20170118193458_add_columns_to_user.rb
@@ -0,0 +1,7 @@
+class AddColumnsToUser < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :city, :string
+    add_column :users, :state, :string
+  end
+end
