4f9cd799e3c88c3d3d9fabda49489a21931af627
sslampa <sslampa@gmail.com>
2017-01-18 11:21:12 -0800

    Added sign up test

--- spec/features/sign_ups_spec.rb
+++ spec/features/sign_ups_spec.rb
@@ -4,11 +4,21 @@ RSpec.feature "SignUps", type: :feature do
   context "I can go to sign up page" do
     Steps "I can go to the landing page and press the 'Sign-up' button" do
       Given "I am on the landing page" do
-        visit '/'
+        visit '/users/sign_up'
       end
-      Then "I can press the 'Sign-up' button" do
-        click_link('Sign-up')
+      Then "I can see the sign up page" do
         expect(page).to have_content "Sign up"
+      end
+      # Then "I can press the 'Sign-up' button and go to sign up page" do
+      #   click_link('Sign-up')
+      #   expect(page).to have_content "Sign up"
+      # end
+      Then "I can fill out form" do
+        fill_in "user[email]", with: "123@yahoo.com"
+        fill_in "user[password]", with: "123456"
+        fill_in "user[password_confirmation]", with: "123456"
+        click_button "Sign up"
+        expect(page).to have_content("Welcome! You have signed up successfully.")
       end
     end
   end
