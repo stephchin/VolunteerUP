e648271201721bb4d65b6e1a7cb2909c563596c6
sslampa <sslampa@gmail.com>
2017-01-18 10:50:28 -0800

    Added basic tests for sign-up/login

---
+++ spec/features/log_ins_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.feature "LogIns", type: :feature do
+  context "I can go to login page" do
+    Steps "I can go to the landing page and press the 'Log in' button" do
+      Given "I am on the landing page" do
+        visit '/'
+      end
+      Then "I can press the 'Log in' button" do
+        click_link('Log in')
+        expect(page).to have_content "Log in"
+      end
+    end
+  end
+end
