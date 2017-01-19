b7e3bd69ff1d87e9327ae7130e53626e24f7f234
sslampa <sslampa@gmail.com>
2017-01-19 11:54:20 -0800

    Fixed test

--- spec/models/user_event_spec.rb
+++ spec/models/user_event_spec.rb
@@ -9,15 +9,18 @@ RSpec.describe UserEvent, type: :model do
 
   describe "User" do
     it "can be created and saved" do
-      u1 = User.create(email: "a@yahoo.com", password: @pw)
-      e1 = Event.create(name: "Blood", start_time: @start, end_time: @end,
+      u1 = User.new(email: "a@yahoo.com", password: @pw, name: "Stephen")
+      e1 = Event.new(name: "Blood", start_time: @start, end_time: @end,
         volunteers_needed: 10)
-      e2 = Event.create(name: "Pestilence", start_time: @start, end_time: @end,
+      e2 = Event.new(name: "Pestilence", start_time: @start, end_time: @end,
         volunteers_needed: 100)
+      expect(u1.save).to eq true
+      expect(e1.save).to eq true
+      expect(e2.save).to eq true
     end
 
     it "can have many events" do
-      u1 = User.create(email: "a@yahoo.com", password: @pw)
+      u1 = User.create(email: "a@yahoo.com", password: @pw, name: "Stephen")
       e1 = Event.create(name: "Blood", start_time: @start, end_time: @end,
         volunteers_needed: 10)
       e2 = Event.create(name: "Pestilence", start_time: @start, end_time: @end,
@@ -32,15 +35,15 @@ RSpec.describe UserEvent, type: :model do
 
     it "can be created and saved" do
       e1 = Event.create(name: "Frost", start_time: @start, end_time: @end)
-      u1 = User.create(email: "b@yahoo.com", password: @pw)
-      u2 = User.create(email: "c@yahoo.com", password: @pw)
+      u1 = User.create(email: "b@yahoo.com", password: @pw, name: "Stephen")
+      u2 = User.create(email: "c@yahoo.com", password: @pw, name: "Stephen")
     end
 
     it "can have many users" do
       e1 = Event.create(name: "Frost", start_time: @start, end_time: @end,
         volunteers_needed: 30)
-      u1 = User.create(email: "b@yahoo.com", password: @pw)
-      u2 = User.create(email: "c@yahoo.com", password: @pw)
+      u1 = User.create(email: "b@yahoo.com", password: @pw, name: "Stephen")
+      u2 = User.create(email: "c@yahoo.com", password: @pw, name: "Stephen")
       expect(e1.user_events.new(user: u1).save).to eq true
       expect(e1.user_events.new(user: u2).save).to eq true
       expect(e1.user_events.all.length).to eq 2
