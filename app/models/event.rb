7ba031da00f27f0cbb1d1ceb8acc436ace49644a
Suzan Sucro <suzansucro@gmail.com>
2017-01-19 10:59:03 -0800

    Add upcoming events to user profile page. Add address method to Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,8 @@ class Event < ApplicationRecord
   validates :start_time, presence: true
   validates :end_time, presence: true
   validates :volunteers_needed, presence: true
+
+  def address
+    "#{street}, #{city} #{state} #{postal_code}"
+  end
 end
