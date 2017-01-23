c179a1200dfadf52c0c01446874bde5fc3fec03d
piercebelton <piercebelton@gmail.com>
2017-01-23 14:44:54 -0800

    fixed testing

--- spec/features/event_pages_spec.rb
+++ spec/features/event_pages_spec.rb
@@ -65,7 +65,7 @@ RSpec.feature "EventPages", type: :feature do
       end
       Then 'When I try to sign up for a full event, there is no volunteer button' do
         click_link (@event.name)
-        expect(page).not_to have_button('Volunteer!')
+        expect(page).to have_button('Volunteer!', disabled: true)
       end
     end
   end
