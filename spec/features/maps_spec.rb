6446695c69efab6b2de158eaf9d845bc32ee37b9
piercebelton <piercebelton@gmail.com>
2017-01-24 14:06:31 -0800

    did google maps testing

--- spec/features/maps_spec.rb
+++ spec/features/maps_spec.rb
@@ -5,7 +5,7 @@ RSpec.feature "Maps", type: :feature do
   before(:each) do
     @organization = Organization.new(name: "ABC", description: "ABC is a helpful org")
     @organization.save
-    @event = Event.create(name: "ABC", start_time:"2017-02-02 01:01:01", end_time:"2017-02-02 02:01:01", volunteers_needed: 1 )
+    @event = Event.create(name: "ABC", start_time:"2017-02-02 01:01:01", end_time:"2017-02-02 02:01:01", volunteers_needed: 1,)
     @event.organization = @organization
     @event.save
     @user = User.find_by_email("a@yahoo.com")
@@ -19,6 +19,12 @@ RSpec.feature "Maps", type: :feature do
       end
       And 'The map has loaded' do
         page.find("#events_map[data-events-id]")
+      end
+      Then 'I can go to the map locations JSON page' do
+        visit '/events/map_locations.json'
+      end
+      And 'I will see the JSON object includes a lat and long' do
+        expect(page).to have_content "#{Event.first.latitude}"
       end
     end
   end
