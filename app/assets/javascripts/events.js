158691845e73bf9deb94780139d003354d97b573
Stephanie Chin <stephjuechin@gmail.com>
2017-01-24 10:29:29 -0800

    Added custom tweet button to event show page

---
+++ app/assets/javascripts/events.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  createTweet();
+
+  function createTweet() {
+    console.log("updateTweet running");
+    var event = $("#event-name").text();
+    var org = $("#organization").text();
+    $("#tweet").attr('data-text', "Excited for "+ org + "'s " + event + "! Join us " + "#volunteerup");
+  }
+
+});
