3a5733a6fe73960dbccf5f92035d0847540b5e54
Suzan Sucro <suzansucro@gmail.com>
2017-01-26 13:51:04 -0800

    User model and feature spec for adding profile images

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,12 +6,13 @@ RSpec.describe User, type: :model do
       expect{User.new}.to_not raise_error
     end
     it "should save to db" do
-      u = User.new(name: "Suzan", email: "123@yahoo.com", password: "123456", city: "San Diego", state: "CA")
+      u = User.new(name: "Suzan", email: "123@yahoo.com", password: "123456", city: "San Diego", state: "CA", image_file_name: "spec/fixtures/default_user.png")
       expect(u.save).to eq true
       expect(u.name).to eq "Suzan"
       expect(u.email).to eq "123@yahoo.com"
       expect(u.city).to eq "San Diego"
       expect(u.state).to eq "CA"
+      expect(u.image_file_name).to eq "spec/fixtures/default_user.png"
     end
   end
 end
