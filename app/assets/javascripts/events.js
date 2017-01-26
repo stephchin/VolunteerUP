5340c37e8e041ae322007e0b23b0720095e11c16
piercebelton <piercebelton@gmail.com>
2017-01-26 09:32:15 -0800

    started to work on user dashboard page maps

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -68,6 +68,22 @@ function createGmapAll(dataFromServer) {
 );
 };
 
+function createGmapUser(dataFromServer) {
+  console.log(dataFromServer);
+  handler = Gmaps.build('Google');
+  handler.buildMap({
+    provider: {},
+    internal: {id: 'events_map'}
+  },
+  function() {
+    showLocations(dataFromServer);
+    markers = handler.addMarkers(dataFromServer);
+    handler.bounds.extendWith(markers);
+    handler.fitMapToBounds();
+  }
+);
+};
+
 function loadAndCreateGmap() {
   if ($("#event_map").length > 0) {
     var eventId = $('#event_map').attr('data-event-id');
@@ -108,7 +124,31 @@ function loadAndCreateGmapAll() {
   }
 };
 
+function loadAndCreateGmapUser() {
+  if ($("#user_map").length > 0) {
+    var myurl = "/events/map_locations";
+    // if ($("#search").val()) {
+    //     myurl += "?search=" + $("#search").val();
+    // };
+
+
+    $.ajax({
+      dataType: 'json',
+      url: myurl,
+      method: 'GET',
+      success: function(dataFromServer) {
+        createGmapUser(dataFromServer)
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        alert("Getting map data failed: " + errorThrown);
+      }
+    });
+  }
+};
+
 $(document).on('ready', loadAndCreateGmap);
 $(document).on('turbolinks:load', loadAndCreateGmap);
 $(document).on('ready', loadAndCreateGmapAll);
 $(document).on('turbolinks:load', loadAndCreateGmapAll);
+$(document).on('ready', loadAndCreateGmapUser);
+$(document).on('turbolinks:load', loadAndCreateGmapUser);
