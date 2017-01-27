3130db11bd7d61396023c5ead407811fba8121bd
Toma Nistor <toma.nistor@gmail.com>
2017-01-27 14:04:12 -0800

    Added search and sort events by org

--- app/models/event.rb
+++ app/models/event.rb
@@ -16,7 +16,6 @@ class Event < ApplicationRecord
   geocoded_by :address
   after_validation :geocode
 
-
   def address
     "#{street}, #{city} #{state} #{postal_code}"
   end
@@ -26,7 +25,7 @@ class Event < ApplicationRecord
   end
 
   filterrific(
-    default_filter_params: { sorted_by: 'created_at_desc' },
+    default_filter_params: { sorted_by: 'event_created_at_desc' },
     available_filters: [
       :sorted_by,
       :search_query,
@@ -44,50 +43,55 @@ class Event < ApplicationRecord
     terms = terms.map { |e|
       ('%' + e.gsub('*', '%') + '%').gsub(/%+/, '%')
     }
-    num_or_conds = 2
+    num_or_conds = 3
     where(
       terms.map { |term|
-        "(LOWER(events.name) LIKE ? OR LOWER(events.cause) LIKE ?)"
+        "(LOWER(events.name) LIKE ? OR LOWER(events.cause) LIKE ? OR LOWER(organizations.name) LIKE ?)"
       }.join(' AND '),
       *terms.map { |e| [e] * num_or_conds }.flatten
-    )
+    ).joins(:organization)
   }
 
   scope :sorted_by, lambda { |sort_option|
     # extract the sort direction from the param value.
     direction = (sort_option =~ /desc$/) ? 'desc' : 'asc'
     case sort_option.to_s
-    when /^created_at_/
-      # Simple sort on the created_at column.
+    when /^event_created_at_/
+      # Simple sort on the event created_at column.
       # Make sure to include the table name to avoid ambiguous column names.
       # Joining on other tables is quite common in Filterrific, and almost
       # every ActiveRecord table has a 'created_at' column.
       order("(events.created_at) #{ direction }")
-    when /^name_/
-      # Simple sort on the name columns
+    when /^event_name_/
+      # Simple sort on the event name columns
       order("LOWER(events.name) #{ direction }")
-    when /^start_time_/
-      # Simple sort on the date column
+    when /^organization_name_/
+      # Simple sort on the organization name columns
+      order("LOWER(organizations.name) #{ direction }").joins(:organization)
+    when /^event_start_time_/
+      # Simple sort on the event date column
       order("(events.start_time)")
     else
       raise(ArgumentError, "Invalid sort option: #{ sort_option.inspect }")
     end
   }
 
-  scope :with_start_time, lambda { |reference_time|
-    where('events.start_time >= ?', reference_time)
-  }
-
-  # always exclude the upper boundary for semi open intervals
-  scope :with_end_time, lambda { |reference_time|
-    where('events.end_time <= ?', reference_time)
-  }
+  # scope :with_start_time, lambda { |reference_time|
+  #   where('events.start_time >= ?', reference_time)
+  # }
+  #
+  # # always exclude the upper boundary for semi open intervals
+  # scope :with_end_time, lambda { |reference_time|
+  #   where('events.end_time <= ?', reference_time)
+  # }
 
   def self.options_for_sorted_by
     [
-      ['Event Name (Ascending)', 'name_asc'],
-      ['Event Name (Descending)', 'name_desc'],
-      ['Date', 'start_time_desc']
+      ['Event Name (Ascending)', 'event_name_asc'],
+      ['Event Name (Descending)', 'event_name_desc'],
+      ['Organization (Ascending)', 'organization_name_asc' ],
+      ['Organization (Descending)', 'organization_name_desc' ],
+      ['Date', 'event_start_time_desc']
     ]
   end
 
