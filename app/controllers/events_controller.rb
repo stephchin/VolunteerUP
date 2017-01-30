3401c12359d5ba30fa6261862e5e5467a96c0e00
Stephanie Chin <stephjuechin@gmail.com>
2017-01-30 10:31:14 -0800

    Fixed events edit form by added orgs_for_select in controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -120,6 +120,10 @@ class EventsController < ApplicationController
   # PATCH/PUT /events/1
   # PATCH/PUT /events/1.json
   def update
+    @organizations_for_select = Organization.all.map do |org|
+      [org.name, org.id]
+    end
+
     respond_to do |format|
       if @event.update(event_params)
         format.html { redirect_to @event, notice: "#{@event.name} was successfully updated!" }
