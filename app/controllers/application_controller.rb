8c1a24184024a40b6f08439445df03f660ebec05
piercebelton <piercebelton@gmail.com>
2017-01-30 16:09:34 -0800

    NOTIFICATIONS WORKgit status ALL TESTS PASSINGgit statusgit statusgit status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_notification
+  # protect_from_forgery with: :exception
   add_flash_types :success, :remove
 
 
 
   protected
+
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :city, :state, :image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:image])
+  end
 
   def set_notification
     if Notification.all.nil?  || !user_signed_in?
@@ -12,11 +20,6 @@ class ApplicationController < ActionController::Base
     else
       @notifications = current_user.notifications.all.reverse
     end
-  end
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :city, :state, :image])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:image])
   end
 
 end
