efe727ecc948f6b56f4e7ac48a66378b4706667e
Stephanie Chin <stephjuechin@gmail.com>
2017-02-07 17:18:22 -0800

    Added paperclip config in development.rb environment

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -7,7 +7,7 @@ class Organization < ApplicationRecord
   validates :name, uniqueness: true, presence: true
   validates :description, presence: true
 
-  has_attached_file :image, styles: { small: "64x64", med: "250x250", large: "400x400" }, default_url: "default_org.png"
+  has_attached_file :image, styles: { small: "64x64", med: "250x250", large: "400x400" }, s3_protocol: :https, default_url: "default_org.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   resourcify
