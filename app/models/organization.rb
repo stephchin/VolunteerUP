c77866d42238d77439f5e62911865bc526e952df
sslampa <sslampa@gmail.com>
2017-02-11 17:37:58 -0800

    #220 Added filtered search for organizations index page

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -13,9 +13,39 @@ class Organization < ApplicationRecord
   :med => "-resize 200x200^ -gravity Center -crop 200x200+0+0 +repage",
   :large => "-resize 500x500^ -gravity Center -crop 500x500+0+0 +repage" },
   default_url: "default_org.png"
-  
+
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
+  filterrific(
+  default_filter_params: { sorted_by: 'organization_name_desc' },
+  available_filters: [
+    :sorted_by,
+    :search_query]
+  )
+
+  scope :search_query, lambda { |query|
+    return nil if query.blank?
+    terms = query.to_s.downcase.split(/\s+/)
+    terms = terms.map { |e|
+      ('%' + e.gsub('*', '%') + '%').sub(/%+/, '%')
+    }
+    num_of_conds = 1
+    where(
+      terms.map { |term|
+        "(LOWER(organizations.name) LIKE ?)"
+      }.join(' AND '),
+      *terms.map { |e| [e] * num_of_conds }.flatten
+    )
+  }
+
+  scope :sorted_by, lambda { |sort_option|
+    direction = (sort_option =~ /desc$/) ? 'desc' : 'asc'
+    case sort_option.to_s
+    when /^organization_name_/
+      order("LOWER(organizations.name) #{ direction }")
+    end
+
+  }
   resourcify
 
 end
